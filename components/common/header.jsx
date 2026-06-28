02d146dfb9cfd9f94e6af7d4c9a955c6a955c8f8
Ayush Juneja <miray.ayushjuneja@gmail.com>
2026-06-29 00:39:05 +0530

    feat: update logo image source in header component

--- components/common/header.jsx
+++ components/common/header.jsx
@@ -42,7 +42,7 @@ export default function Header({ toggleSidebar }) {
         className="absolute left-1/2 -translate-x-1/2 flex items-center gap-2"
       >
         <Image
-          src="https://www.mirayfashions.com/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdjtva6hec%2Fimage%2Fupload%2Fv1764916639%2Fmiray%2Fmedia%2Fk0yvgu5m0ij1husm3ugh.png&w=256&q=75"
+          src="https://res.cloudinary.com/djtva6hec/image/upload/v1782673709/miray/media/ygzojdrhurqfnuwxvm7m.webp"
           alt="Logo"
           width={110}
           height={42}
@@ -50,7 +50,7 @@ export default function Header({ toggleSidebar }) {
           style={{ width: "110px", height: "auto" }} // ✅ keep ratio
         />
 
-        
+
       </button>
 
       {/* Right Section (Profile dropdown) */}
@@ -64,7 +64,7 @@ export default function Header({ toggleSidebar }) {
 
         {open && (
           <div className="absolute right-0 mt-2 w-48 bg-white shadow-md rounded-xl border border-gray-200 py-2 animate-fadeIn">
-            
+
             {/* ✅ Profile Option */}
             <button
               onClick={() => {
